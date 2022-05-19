1e0007ba233019a12f6b695bc3e495d1905c1ef8
JeffUlan <nakamotoakira19940929@gmail.com>
2022-05-19 17:56:21 +0545

    animate intro letters in home page

---
+++ src/components/AnimatedLetters/AnimatedLetters.jsx
@@ -0,0 +1,14 @@
+import "./animateletters.css";
+const AnimatedLetters = ({ letterClass, strArray, index }) => {
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + index}`}>
+          {char}
+        </span>
+      ))}
+    </span>
+  );
+};
+
+export default AnimatedLetters;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import LogoTitle from "../../assets/images/logo-s.png";
+import AnimatedLetters from "../../components/AnimatedLetters/AnimatedLetters";
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("textAnimate");
+  const nameArray = ["u", "s", "h", "a", "n", "t"];
+  const jobArray = [
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+    ".  ",
+  ];
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("textAnimateHover");
+    }, 4000);
+  }, []);
   return (
-    <div className="homePage">
-      <div className="absolute left-[10%] top-[50%] -translate-y-[50%] w-[40%] max-h-[90%]">
+    <div>
+      <div className="homePage absolute left-[10%] top-[50%] -translate-y-[50%] w-[40%] max-h-[90%]">
         <h1 className="text-white text-[56px] leading-[53px] m-0 font-coolvetica font-normal">
-          Hi, <br />
-          <span className="flex">
-            I'm
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i</span>
+          <span className={`${letterClass} _13`}>,</span> <br />
+          <span className="flex items-center">
+            <span className={`${letterClass} _14`}>I</span>
+            <span className={`${letterClass} _15`}>'m</span>
             <img
               src={LogoTitle}
               alt="JavaScript Developer Name, Web Developer Namer"
             />
-            ushant
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              index={15}
+            />
           </span>
-          web developer.
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            index={22}
+          />
         </h1>
         <h2>Frontend Developer | Javascript Programmer</h2>
         <Link to="/contact" className="flatButton">
