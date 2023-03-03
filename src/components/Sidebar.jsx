31474c576b0d1eda1418ea5ec1895a8c34f02be2
JeffUlan <nakamotoakira19940929@gmail.com>
2023-03-03 23:52:06 +0545

    style(sidebar): fix sidebar layout issue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,6 +9,7 @@ import {
   faUser,
   faCode,
   faBriefcase,
+  faClose,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faLinkedin,
@@ -19,7 +20,7 @@ import {
 const Sidebar = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <header className="bg-sidebar w-full h-28 lg:w-24 lg:h-screen lg:fixed top-0 z-10 flex flex-row lg:flex-col items-center lg:gap-x-10 relative ">
+    <header className="bg-sidebar w-full h-28 lg:w-24 lg:h-screen lg:fixed top-0 z-10 flex flex-row lg:flex-col items-center lg:gap-x-10 lg:justify-between relative ">
       <Link to="/">
         <div className="">
           <img
@@ -33,7 +34,7 @@ const Sidebar = () => {
         </div>
       </Link>
       {/* navbar section */}
-      <nav className="lg:mt-72 md:flex flex-row lg:flex-col items-center md:mx-auto gap-x-10 md:gap-x-20 lg:gap-y-10 hidden">
+      <navbar className="md:flex flex-row lg:flex-col items-center md:mx-auto gap-x-10 md:gap-x-20 lg:gap-y-10 hidden">
         <NavLink
           exact="true"
           activeclassname="selected"
@@ -105,136 +106,139 @@ const Sidebar = () => {
             className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
           />
         </NavLink>
-        <ul className="flex lg:flex-col items-center gap-x-12 lg:gap-y-8 absolute lg:bottom-16 right-14 lg:right-8">
-          <li>
-            <a
-              href="https://www.linkedin.com/in/sushant-dhimal-809456202/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                className="text-icon hover:text-secondary"
-                size="2x"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/dhiant"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faGithub}
-                className="text-icon hover:text-secondary"
-                size="2x"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/shant_dhi/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faInstagram}
-                className="text-icon hover:text-secondary"
-                size="2x"
-              />
-            </a>
-          </li>
-        </ul>
-      </nav>
+      </navbar>
+      <ul className="hidden md:flex lg:flex-col items-center gap-x-12 lg:gap-y-8 lg:mb-5">
+        <li>
+          <a
+            href="https://www.linkedin.com/in/sushant-dhimal-809456202/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="text-icon hover:text-secondary"
+              size="2x"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/dhiant" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="text-icon hover:text-secondary"
+              size="2x"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/shant_dhi/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="text-icon hover:text-secondary"
+              size="2x"
+            />
+          </a>
+        </li>
+      </ul>
       {/* collapsable button */}
       <div className="absolute top-7 right-8 md:hidden">
         <button onClick={() => setToggle(!toggle)}>
-          <FontAwesomeIcon icon={faBars} className="text-secondary" size="3x" />
+          <FontAwesomeIcon
+            icon={!toggle ? faBars : faClose}
+            className="text-secondary"
+            size="3x"
+          />
         </button>
       </div>
+
       {toggle && (
         <div
-          className="bg-sidebar w-full h-20 absolute top-28 flex items-center justify-evenly gap-14 menuButtons z-10
+          className="md:hidden bg-sidebar px-4 w-full h-20 absolute top-28 flex flex-wrap gap-y-10 items-center justify-between menuButtons z-10
         "
         >
-          <NavLink
-            exact="true"
-            activeclassname="selected"
-            to="/"
-            style={({ isActive }) => ({
-              color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
-            })}
-          >
-            <FontAwesomeIcon
-              icon={faHome}
-              size="2x"
-              className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
-              onClick={() => setToggle(false)}
-            />
-          </NavLink>
-          <NavLink
-            exact="true"
-            activeclassname="selected"
-            to="/about"
-            style={({ isActive }) => ({
-              color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
-            })}
-          >
-            <FontAwesomeIcon
-              icon={faUser}
-              size="2x"
-              className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
-              onClick={() => setToggle(false)}
-            />
-          </NavLink>
-          <NavLink
-            exact="true"
-            activeclassname="selected"
-            to="/contact"
-            style={({ isActive }) => ({
-              color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
-            })}
-          >
-            <FontAwesomeIcon
-              icon={faEnvelope}
-              size="2x"
-              className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
-              onClick={() => setToggle(false)}
-            />
-          </NavLink>
-          <NavLink
-            exact="true"
-            activeclassname="selected"
-            to="/works"
-            style={({ isActive }) => ({
-              color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
-            })}
-          >
-            <FontAwesomeIcon
-              icon={faBriefcase}
-              size="2x"
-              className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
-              onClick={() => setToggle(false)}
-            />
-          </NavLink>
-          <NavLink
-            exact="true"
-            activeclassname="selected"
-            to="/skills"
-            style={({ isActive }) => ({
-              color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
-            })}
-          >
-            <FontAwesomeIcon
-              icon={faCode}
-              size="2x"
-              className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
-              onClick={() => setToggle(false)}
-            />
-          </NavLink>
+          <div className="flex gap-x-14">
+            <NavLink
+              exact="true"
+              activeclassname="selected"
+              to="/"
+              style={({ isActive }) => ({
+                color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
+              })}
+            >
+              <FontAwesomeIcon
+                icon={faHome}
+                size="2x"
+                className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
+                onClick={() => setToggle(false)}
+              />
+            </NavLink>
+            <NavLink
+              exact="true"
+              activeclassname="selected"
+              to="/about"
+              style={({ isActive }) => ({
+                color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
+              })}
+            >
+              <FontAwesomeIcon
+                icon={faUser}
+                size="2x"
+                className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
+                onClick={() => setToggle(false)}
+              />
+            </NavLink>
+            <NavLink
+              exact="true"
+              activeclassname="selected"
+              to="/contact"
+              style={({ isActive }) => ({
+                color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
+              })}
+            >
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                size="2x"
+                className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
+                onClick={() => setToggle(false)}
+              />
+            </NavLink>
+            <NavLink
+              exact="true"
+              activeclassname="selected"
+              to="/works"
+              style={({ isActive }) => ({
+                color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
+              })}
+            >
+              <FontAwesomeIcon
+                icon={faBriefcase}
+                size="2x"
+                className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
+                onClick={() => setToggle(false)}
+              />
+            </NavLink>
+            <NavLink
+              exact="true"
+              activeclassname="selected"
+              to="/skills"
+              style={({ isActive }) => ({
+                color: isActive ? "var(--c-secondary)" : "var(--c-icon)",
+              })}
+            >
+              <FontAwesomeIcon
+                icon={faCode}
+                size="2x"
+                className="fill-current hover:text-secondary transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-150 duration-300"
+                onClick={() => setToggle(false)}
+              />
+            </NavLink>
+          </div>
           {/* social accounts */}
-          <ul className="hidden sm:flex lg:flex-col items-center gap-x-12 lg:gap-y-8 absolute lg:bottom-16 right-14 lg:right-8">
+          <ul className="bg-sidebar w-full flex gap-x-10 pb-5">
             <li>
               <a
                 href="https://www.linkedin.com/in/sushant-dhimal-809456202/"
@@ -261,7 +265,7 @@ const Sidebar = () => {
                 />
               </a>
             </li>
-            <li>
+            <li className="hidden xs:block">
               <a
                 href="https://www.instagram.com/shant_dhi/"
                 target="_blank"
